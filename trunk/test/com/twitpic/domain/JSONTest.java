d100c9c0caeaa527a670a1c056777b6fd12efce1
yan925 <yan925@gmail.com>
2009-08-18 13:34:56 +0000

    add json convert in MODE PicturesParameter.java

--- trunk/test/com/twitpic/domain/JSONTest.java
+++ trunk/test/com/twitpic/domain/JSONTest.java
@@ -1,6 +1,12 @@
 package com.twitpic.domain;
 
+import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+import net.sf.json.JSONObject;
+
 import com.twitpic.db.model.PicturesParameter;
 
 public class JSONTest {
@@ -10,7 +16,10 @@ public class JSONTest {
 		
 		PicturesParameter p = new PicturesParameter();
 		p.setUploadTime(new java.util.Date());
-		p.setFormat(new SimpleDateFormat("yyyy年MM-dd kk:mm:ss"));
-		System.out.println(p.to_json());
+		p.setFormat(new SimpleDateFormat("yyyy年MM-dd kk:mm:ss"));		
+		String json_str = p.to_json().toString();
+		JSONObject jsonObject = JSONObject.fromObject(json_str);      
+		System.out.println(jsonObject.get("uploadTime"));
+		Object bean = JSONObject.toBean( jsonObject );
 	}
 }
