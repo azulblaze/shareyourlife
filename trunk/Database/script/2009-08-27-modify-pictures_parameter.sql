be9f428f61d27f6451c7a024d436ccb7f4a097e8
yan925 <yan925@gmail.com>
2009-08-27 14:47:18 +0000

    PicturesParameter:add device to this table. add Database dictionary ! before you run this APP, you need to run the db package script!

---
+++ trunk/Database/script/2009-08-27-modify-pictures_parameter.sql
@@ -0,0 +1,19 @@
+//add device to pictures_parameter
+rename table pictures_parameter to pictures_parameter_tmp;
+go
+CREATE  TABLE IF NOT EXISTS pictures_parameter (
+  id_pictures BIGINT NOT NULL ,
+  upload_account VARCHAR(100) NOT NULL ,
+  upload_time TIMESTAMP NULL ,
+  status INT NULL DEFAULT 1 ,
+  device VARCHAR(50) NULL,
+  title VARCHAR(100) NULL ,
+  description VARCHAR(1000) NULL ,
+  addtion_info TEXT NULL ,
+  PRIMARY KEY (id_pictures) )
+ENGINE = InnoDB;
+go
+INSERT pictures_parameter (id_pictures, upload_account, upload_time,status,title,description,addtion_info) select * from pictures_parameter_tmp;
+go
+drop table pictures_parameter_tmp;
+go
