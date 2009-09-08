5ed5e79d0b12523d44f722fd7b8a6b1af69866c3
yan925@gmail.com <yan925@gmail.com@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2009-09-08 09:35:34 +0000

    follow table finished

--- trunk/Database/script/create.sql
+++ trunk/Database/script/create.sql
@@ -276,3 +276,19 @@ CREATE  TABLE IF NOT EXISTS users_group (
   INDEX I_GROUP (id_groups ASC) )
 ENGINE = InnoDB
 go
+
+-- -----------------------------------------------------
+-- Table follows
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS follows ;
+go
+CREATE  TABLE IF NOT EXISTS follows (
+  id BIGINT NOT NULL AUTO_INCREMENT ,
+  follow VARCHAR(100) NULL ,
+  followed VARCHAR(100) NULL ,
+  function VARCHAR(45) NULL ,
+  status INT NULL ,
+  update_time TIMESTAMP NULL DEFAULT current_timestamp ,
+  PRIMARY KEY (id) )
+ENGINE = InnoDB
+go
