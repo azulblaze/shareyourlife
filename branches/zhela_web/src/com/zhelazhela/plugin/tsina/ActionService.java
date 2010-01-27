e4ee7d66737e102fbea14708114bb31ff4ac672a
yan925@gmail.com <yan925@gmail.com@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-01-27 01:43:49 +0000

    remove sina

--- branches/zhela_web/src/com/zhelazhela/plugin/tsina/ActionService.java
+++
@@ -1,8 +0,0 @@
-package com.zhelazhela.plugin.tsina;
-
-public interface ActionService {
-	
-	public final static String INDEX = "http://t.sina.com.cn/";
-	
-	public boolean login(String name, String password);
-}
