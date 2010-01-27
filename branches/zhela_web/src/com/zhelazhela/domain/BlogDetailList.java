fc9fb988c39d101346f4500fd1e39af7dd064a2a
yan925@gmail.com <yan925@gmail.com@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-01-27 06:10:59 +0000

    add domain model base class

--- branches/zhela_web/src/com/zhelazhela/domain/BlogDetailList.java
+++ branches/zhela_web/src/com/zhelazhela/domain/BlogDetailList.java
@@ -1,6 +1,20 @@
 package com.zhelazhela.domain;
 
-public class BlogDetailList {
+import java.util.List;
+
+import com.zhelazhela.db.model.BlogDetail;
+
+public class BlogDetailList extends BaseList{
 	
+	private List<BlogDetail> list;
+
+	public List<BlogDetail> getList() {
+		return list;
+	}
+
+	public void setList(List<BlogDetail> list) {
+		this.list = list;
+		this.c_size = list.size();
+	}
 	
 }
--- branches/zhela_web/src/com/zhelazhela/services/BlogService.java
+++ branches/zhela_web/src/com/zhelazhela/services/BlogService.java
@@ -1,6 +1,7 @@
 package com.zhelazhela.services;
 
 import com.zhelazhela.db.model.BlogDetail;
+import com.zhelazhela.domain.BlogDetailList;
 
 public interface BlogService {
 	
@@ -9,6 +10,8 @@ public interface BlogService {
 	public BlogDetail loadBlogDetail(long id) throws Exception;
 	
 	public BlogDetail updateBlogDetail(BlogDetail record) throws Exception;
+	
+	public BlogDetailList loadList(int page,int pagesize,String category,java.util.Date start,java.util.Date end) throws Exception;
 	
 	
 }
