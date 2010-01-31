1d2df26920e89ff5df533b475079bc70286d5edf
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-01-31 14:25:21 +0000

    blog done

--- src/com/zhelazhela/services/BlogService.java
+++ src/com/zhelazhela/services/BlogService.java
@@ -1,26 +1,47 @@
 package com.zhelazhela.services;
 
+import com.zhelazhela.db.model.Attachments;
 import com.zhelazhela.db.model.BlogComments;
 import com.zhelazhela.db.model.BlogDetail;
+import com.zhelazhela.db.model.BlogQa;
+import com.zhelazhela.db.model.Tags;
+import com.zhelazhela.domain.AttachmentList;
 import com.zhelazhela.domain.BlogDetailList;
+import com.zhelazhela.domain.BlogQaList;
 
 public interface BlogService {
 	
-	public BlogDetail saveBlog(BlogDetail record) throws Exception;
+	public BlogDetail saveBlog(BlogDetail record,String tags) throws Exception;
 	
 	public BlogDetail loadBlogDetail(long id) throws Exception;
 	
-	public BlogDetail updateBlogDetail(BlogDetail record) throws Exception;
+	public BlogDetail updateBlogDetail(BlogDetail record,String tags) throws Exception;
 	
-	public BlogDetailList loadList(int page,int pagesize,String category,java.util.Date start,java.util.Date end) throws Exception;
+	public BlogDetailList loadList(int page,int pagesize,String category,java.util.Date start,java.util.Date end,Boolean published,Boolean staticed) throws Exception;
 	
 	public BlogDetailList loadList(int page,int pagesize,String tag) throws Exception;
 	
-	public boolean publishBlog(long id) throws Exception;
+	public boolean publishBlog(long id,String rootpath) throws Exception;
 	
 	public boolean delBlog(long id) throws Exception;
 	
 	public BlogComments comment(BlogComments record) throws Exception;
 	
 	public java.util.List<BlogComments> loadComment(long id) throws Exception;
+	
+	public java.util.List<Tags> loadTopTags(int size) throws Exception;
+	
+	public java.util.List<String> loadCategorys() throws Exception;
+	
+	public void Question(BlogQa bq) throws Exception;
+	
+	public BlogQaList loadBlogQaList(int page,int pagesize) throws Exception;
+	
+	public boolean delQuestion(long id) throws Exception;
+	
+	public Attachments uploadPic(Attachments am, String path, String ext_type, java.io.File pic) throws Exception;
+	
+	public boolean delPic(long id,String path) throws Exception;
+	
+	public AttachmentList loadBlogPic(int page,int pagesize) throws Exception;
 }
