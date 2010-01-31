6409f177a36102f32425a1808ce8d3ae0582df97
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-01-31 14:25:14 +0000

    blog done

--- src/com/zhelazhela/domain/BaseList.java
+++ src/com/zhelazhela/domain/BaseList.java
@@ -3,12 +3,25 @@ package com.zhelazhela.domain;
 public class BaseList {
 	/** 当前页数 */
 	protected int page;
+	/** 总页数 */
+	protected int allpage;
 	/** 每页数量 */
 	protected int pagesize;
 	/** 总数量 */
 	protected int size;
 	
 	protected int c_size;
+	
+	protected void countPage(){
+		if(pagesize>0&&size>0){
+			this.allpage = size/pagesize;
+			if(size%pagesize>0){
+				this.allpage = this.allpage + 1;
+			}
+		}else{
+			allpage = 0;
+		}
+	}
 
 	public int getPage() {
 		return page;
@@ -18,12 +31,21 @@ public class BaseList {
 		this.page = page;
 	}
 
+	public int getAllpage() {
+		return allpage;
+	}
+
+	public void setAllpage(int allpage) {
+		this.allpage = allpage;
+	}
+
 	public int getPagesize() {
 		return pagesize;
 	}
 
 	public void setPagesize(int pagesize) {
 		this.pagesize = pagesize;
+		countPage();
 	}
 
 	public int getSize() {
@@ -32,6 +54,7 @@ public class BaseList {
 
 	public void setSize(int size) {
 		this.size = size;
+		countPage();
 	}
 
 	public int getC_size() {
