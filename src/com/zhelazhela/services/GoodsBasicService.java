88ee206c5a9aabfb0f9511af8552eada70e63020
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-02-22 15:30:40 +0000

    end comment load

--- src/com/zhelazhela/services/GoodsBasicService.java
+++ src/com/zhelazhela/services/GoodsBasicService.java
@@ -1,12 +1,18 @@
 package com.zhelazhela.services;
 
+import com.zhelazhela.domain.GoodCommentList;
 import com.zhelazhela.domain.GoodsCollection;
 import com.zhelazhela.domain.GoodsDetail;
 import com.zhelazhela.domain.SNSUser;
+import com.zhelazhela.domain.UserTrackList;
 
 public interface GoodsBasicService {
 
 	public GoodsDetail addSiteGoods(GoodsCollection gc) throws Exception;
 		
 	public GoodsDetail loadGoodsDetail(long id,SNSUser user,String sn,int page,int pagesize) throws Exception;
+	
+	public UserTrackList loadUserTrack(long id,SNSUser user, String sn,int page,int pagesize) throws Exception;
+	
+	public GoodCommentList loadUserComment(long id,String sn,int page,int pagesize) throws Exception;
 }
