d76196d801c3b750644455d4e4fadc4e480d15ec
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-02-22 15:30:24 +0000

    end comment load

--- src/com/zhelazhela/domain/GoodCommentList.java
+++ src/com/zhelazhela/domain/GoodCommentList.java
@@ -1,5 +1,19 @@
 package com.zhelazhela.domain;
 
+import com.zhelazhela.db.model.define.UserComment;
+
 public class GoodCommentList extends BaseList {
 
+	private java.util.List<UserComment> list;
+
+	public java.util.List<UserComment> getList() {
+		return list;
+	}
+
+	public void setList(java.util.List<UserComment> list) {
+		this.list = list;
+		this.c_size = list.size();
+	}
+	
+	
 }
