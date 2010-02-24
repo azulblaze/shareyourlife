a1ce4c8a5213f56f6b906035c38dc0ce82a5e1e5
yan925 <yan925@gmail.com>
2010-02-24 02:37:33 +0000

    rename goods offer list

--- src/com/zhelazhela/domain/GoodsOfferList.java
+++ src/com/zhelazhela/domain/GoodsOfferList.java
@@ -1,5 +1,16 @@
 package com.zhelazhela.domain;
 
-public class GoodsOfferList extends BaseList {
+import com.zhelazhela.db.model.define.UserPrice;
 
+public class GoodsOfferList extends BaseList {
+	private java.util.List<UserPrice> list;
+
+	public java.util.List<UserPrice> getList() {
+		return list;
+	}
+
+	public void setList(java.util.List<UserPrice> list) {
+		this.list = list;
+		this.c_size = list.size();
+	}
 }
