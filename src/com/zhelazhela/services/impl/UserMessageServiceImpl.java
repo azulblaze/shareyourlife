3e9433bf396de93acab0b1961f1e88a6415bb5ef
yan925 <yan925@gmail.com>
2010-03-02 09:35:44 +0000

    2010/3/2 company

--- src/com/zhelazhela/services/impl/UserMessageServiceImpl.java
+++ src/com/zhelazhela/services/impl/UserMessageServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.zhelazhela.db.dao.InboxMessageDAO;
 import com.zhelazhela.db.dao.OutboxMessageDAO;
+import com.zhelazhela.db.dao.UserDAO;
 import com.zhelazhela.db.model.InboxMessage;
 import com.zhelazhela.db.model.InboxMessageExample;
 import com.zhelazhela.db.model.OutboxMessage;
@@ -16,6 +17,8 @@ public class UserMessageServiceImpl implements UserMessageService {
 	
 	private OutboxMessageDAO outboxMessageDAO;
 	
+	private UserDAO userDAO;
+	
 	public void setInboxMessageDAO(InboxMessageDAO inboxMessageDAO) {
 		this.inboxMessageDAO = inboxMessageDAO;
 	}
@@ -24,6 +27,12 @@ public class UserMessageServiceImpl implements UserMessageService {
 		this.outboxMessageDAO = outboxMessageDAO;
 	}
 
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	
+	
 	@Override
 	public InboxMessage readInBoxMessage(long id) {
 		return inboxMessageDAO.selectByPrimaryKey(id);
@@ -38,6 +47,9 @@ public class UserMessageServiceImpl implements UserMessageService {
 	public OutboxMessage sendMessage(long from, long to, String subject,
 			int type, String status, String parameter, String message)
 			throws Exception {
+		if(userDAO.selectByPrimaryKey(to)==null){
+			throw new Exception("该用户不存在");
+		}
 		InboxMessage im = new InboxMessage();
 		im.setFromId(from);
 		im.setUserId(to);
@@ -64,7 +76,7 @@ public class UserMessageServiceImpl implements UserMessageService {
 	}
 
 	@Override
-	public List<InboxMessage> loadInbox(long userId, int page, int pagesize) {
+	public List<InboxMessage> loadInbox(long userId, int page, int pagesize) throws Exception {
 		InboxMessageExample example = new InboxMessageExample();
 		example.createCriteria().andUserIdEqualTo(userId).andIsDeletedEqualTo(false);
 		example.setOrderByClause("update_time asc");
@@ -75,7 +87,7 @@ public class UserMessageServiceImpl implements UserMessageService {
 	}
 
 	@Override
-	public List<OutboxMessage> loadOutbox(long userId, int page, int pagesize) {
+	public List<OutboxMessage> loadOutbox(long userId, int page, int pagesize) throws Exception {
 		OutboxMessageExample example = new OutboxMessageExample();
 		example.createCriteria().andUserIdEqualTo(userId).andIsDeletedEqualTo(false);
 		example.setOrderByClause("update_time asc");
@@ -86,7 +98,7 @@ public class UserMessageServiceImpl implements UserMessageService {
 	}
 
 	@Override
-	public void clearInbox(long userId) {
+	public void clearInbox(long userId) throws Exception {
 		InboxMessageExample example = new InboxMessageExample();
 		example.createCriteria().andUserIdEqualTo(userId).andIsDeletedEqualTo(false);
 		InboxMessage record = new InboxMessage();
@@ -96,7 +108,7 @@ public class UserMessageServiceImpl implements UserMessageService {
 	}
 
 	@Override
-	public void clearOutbox(long userId) {
+	public void clearOutbox(long userId) throws Exception {
 		OutboxMessageExample example = new OutboxMessageExample();
 		example.createCriteria().andUserIdEqualTo(userId).andIsDeletedEqualTo(false);
 		OutboxMessage record = new OutboxMessage();
@@ -106,7 +118,7 @@ public class UserMessageServiceImpl implements UserMessageService {
 	}
 
 	@Override
-	public void delInbox(List<Long> ids) {
+	public void delInbox(List<Long> ids) throws Exception {
 		InboxMessageExample example = new InboxMessageExample();
 		example.createCriteria().andIdIn(ids).andIsDeletedEqualTo(false);
 		InboxMessage record = new InboxMessage();
@@ -117,13 +129,59 @@ public class UserMessageServiceImpl implements UserMessageService {
 	}
 
 	@Override
-	public void delOutbox(List<Long> ids) {
+	public void delOutbox(List<Long> ids) throws Exception {
 		OutboxMessageExample example = new OutboxMessageExample();
 		example.createCriteria().andIdIn(ids).andIsDeletedEqualTo(false);
 		OutboxMessage record = new OutboxMessage();
 		record.setIsDeleted(true);
 		record.setUpdateTime(new java.util.Date());
 		outboxMessageDAO.updateByExampleSelective(record, example);
+	}
+
+	@Override
+	public void sendAllUser() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendCareApply(long source, long dest, String content)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendFriend(long source, long dest, String content)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendGroupApply(long source, long groupid, String content)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendGroupInvition(long source, long groupid, long userId,
+			String content) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendGroupMembers(long userId, long groupId, String content)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void sendUsers(List<Long> userIds) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
