7173335f8d648e4bc1748ea10d7c6bb36b21eeb4
yan925 <yan925@gmail.com>
2010-03-02 09:35:44 +0000

    2010/3/2 company

---
+++ branches/zhela_web/src/com/zhelazhela/db/model/define/MessageStatus.java
@@ -0,0 +1,21 @@
+package com.zhelazhela.db.model.define;
+
+public class MessageStatus {
+
+	/**
+	 * 标记消息刚发送,还未被阅读
+	 */
+	public static final String SEND = "send";
+	/**
+	 * 标记消息已经被阅读过了
+	 */
+	public static final String READ = "read";
+	/**
+	 * 标记消息被回复过了
+	 */
+	public static final String REPLY = "reply";
+	/**
+	 * 标记消息被处理过了(主要针对好友申请,组申请)
+	 */
+	public static final String DONE = "done";
+}
---
+++ branches/zhela_web/src/com/zhelazhela/db/model/define/MessageType.java
@@ -0,0 +1,26 @@
+package com.zhelazhela.db.model.define;
+
+public class MessageType {
+	
+	/**
+	 * 普通消息
+	 */
+	public final static int GENERAL = 0;
+	/**
+	 * 申请好友消息
+	 */
+	public final static int APPLY_CARE = 1;
+	/**
+	 * 申请加入组消息
+	 */
+	public final static int APPLY_GROUP = 2;
+	/**
+	 * 邀请加入组消息
+	 */
+	public final static int IVENT_GROUP = 3;
+	/**
+	 * 拒绝信息
+	 */
+	public final static int BLOCK = 4;
+	
+}
