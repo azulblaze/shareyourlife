98cb67943a6c0181b317165c2bfdd6297591c73d
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-08-31 14:35:21 +0000

    finished service interface

--- branches/zhela_web/src/com/zhelazhela/cloudblog/services/impl/MessageServiceImpl.java
+++ branches/zhela_web/src/com/zhelazhela/cloudblog/services/impl/MessageServiceImpl.java
@@ -1,7 +1,36 @@
 package com.zhelazhela.cloudblog.services.impl;
 
+import java.util.Map;
+
+import com.zhelazhela.cloudblog.domain.ACK;
+import com.zhelazhela.cloudblog.domain.MessageList;
+import com.zhelazhela.cloudblog.domain.MessageResult;
 import com.zhelazhela.cloudblog.services.MessageService;
 
 public class MessageServiceImpl implements MessageService {
+
+	@Override
+	public ACK del(Map<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MessageList inbox(Map<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MessageList outbox(Map<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MessageResult sendmsg(Map<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
