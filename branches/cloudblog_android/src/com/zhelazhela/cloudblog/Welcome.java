8a7c8049af480bd0cd5b670f26bfef7d2e429363
yan925 <yan925@gmail.com>
2010-09-14 09:57:45 +0000

    round corner is finished

--- branches/cloudblog_android/src/com/zhelazhela/cloudblog/Welcome.java
+++ branches/cloudblog_android/src/com/zhelazhela/cloudblog/Welcome.java
@@ -2,12 +2,16 @@ package com.zhelazhela.cloudblog;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Bitmap.Config;
+import android.graphics.PorterDuff.Mode;
 import android.os.Bundle;
 import android.text.Html;
 import android.widget.ImageView;
@@ -21,27 +25,31 @@ public class Welcome extends Activity {
         setContentView(R.layout.microblog);
         TextView tv = (TextView)findViewById(R.id.blog_text);
         tv.setBackgroundColor(3284683);
-        tv.setText(Html.fromHtml("<a href='http://google.com.hk'>Hello</a>,Can weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ support an weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ an weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ an weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ an weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ an weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ an weÊÕ¿î»ú·¢»ÓÈö¿Æ´òÚ»·¢µçÊÓ¼à¿Ø·¢ link?"));
+        tv.setText(Html.fromHtml("<a href='http://google.com.hk'>Hello</a>,Can weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ support an weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ an weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ an weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ an weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ an weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ an weï¿½Õ¿ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Æ´ï¿½Ú»ï¿½ï¿½ï¿½ï¿½ï¿½Ó¼ï¿½Ø·ï¿½ link?"));
         ImageView iv = (ImageView)findViewById(R.id.header);
-        drawRounderIMG(iv.getDrawingCache());
+        Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.header);
+        iv.setImageBitmap(getRoundedCornerBitmap(bMap));
     }
     
-    private void drawRounderIMG(Bitmap myCoolBitmap){
-    	int w = myCoolBitmap.getWidth(), h = myCoolBitmap.getHeight();
-    	// We have to make sure our rounded corners have an alpha channel in most cases
-    	Bitmap rounder = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
-    	Canvas canvas = new Canvas(rounder);    
-    	// We're going to apply this paint eventually using a porter-duff xfer mode.
-    	// This will allow us to only overwrite certain pixels. RED is arbitrary. This
-    	// could be any color that was fully opaque (alpha = 255)
-    	Paint xferPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    	xferPaint.setColor(Color.RED);
-    	// We're just reusing xferPaint to paint a normal looking rounded box, the 20.f
-    	// is the amount we're rounding by.
-    	canvas.drawRoundRect(new RectF(0,0,w,h), 20.0f, 20.0f, xferPaint);     
-    	// Now we apply the 'magic sauce' to the paint  
-    	xferPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-
-    }
+    private Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+            bitmap.getHeight(), Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+     
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+     
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+     
+        return output;
+      }
     
 }
