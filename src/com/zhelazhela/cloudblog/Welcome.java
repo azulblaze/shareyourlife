d1695a819f067c0babff7de57b1c5bba2a65fedd
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-09-16 18:47:21 +0000

    finished listview

--- src/com/zhelazhela/cloudblog/Welcome.java
+++ src/com/zhelazhela/cloudblog/Welcome.java
@@ -1,55 +1,34 @@
 package com.zhelazhela.cloudblog;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.zhelazhela.cloudblog.ui.BlogDetailAdapter;
+
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.Bitmap.Config;
-import android.graphics.PorterDuff.Mode;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Html;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ListView;
 
 public class Welcome extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.microblog);
-        TextView tv = (TextView)findViewById(R.id.blog_text);
-        tv.setBackgroundColor(3284683);
-        tv.setText(Html.fromHtml("<a href='http://google.com.hk'>Hello</a>,Can we�տ������ƴ�ڻ�����Ӽ�ط� support an we�տ������ƴ�ڻ�����Ӽ�ط� an we�տ������ƴ�ڻ�����Ӽ�ط� an we�տ������ƴ�ڻ�����Ӽ�ط� an we�տ������ƴ�ڻ�����Ӽ�ط� an we�տ������ƴ�ڻ�����Ӽ�ط� an we�տ������ƴ�ڻ�����Ӽ�ط� link?"));
-        ImageView iv = (ImageView)findViewById(R.id.header);
-        Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.header);
-        iv.setImageBitmap(getRoundedCornerBitmap(bMap));
+        setContentView(R.layout.bloglist);
+        ListView sv = (ListView)findViewById(R.id.blogs_container_list);
+        ArrayList<HashMap<String, String>> mylist = new java.util.ArrayList<java.util.HashMap<String, String>>();
+        for(int i=0;i<10;i++){
+        	java.util.HashMap<String, String> map = new java.util.HashMap<String, String>();
+        	map.put("blog_text", "<a href='google.com'>google.com</a>Tis i第十六发山东龙口附件乐山大佛斯蒂芬就阿里山等级分 临时单据反动势力发上篮得分技术大力开发 临时单据费力的事发乐山大佛降低税率辅导书 s ftesfsldkfjsdlkf"+i);
+        	map.put("blog_from", "This is from"+i);
+        	mylist.add(map);
+        }
+        BlogDetailAdapter bda = new BlogDetailAdapter(this,mylist);
+        sv.setAdapter(bda);
     }
     
-    private Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-            bitmap.getHeight(), Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-     
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-        final RectF rectF = new RectF(rect);
-        final float roundPx = 24;
-     
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-     
-        return output;
-      }
+    
     
 }
