027b781f9a38ebc7ff284a85105dd18762858f49
yan925 <yan925@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-09-18 10:43:13 +0000

    3 activity

--- src/com/zhelazhela/cloudblog/ui/BlogDetailAdapter.java
+++ src/com/zhelazhela/cloudblog/ui/BlogDetailAdapter.java
@@ -1,38 +1,76 @@
 package com.zhelazhela.cloudblog.ui;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import com.zhelazhela.cloudblog.BlogList;
 import com.zhelazhela.cloudblog.R;
 import com.zhelazhela.cloudblog.domain.Post;
+import com.zhelazhela.cloudblog.service.AsyncImageLoader;
+import com.zhelazhela.cloudblog.service.AsyncImageLoader.ImageCallback;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.RectF;
-import android.graphics.Bitmap.Config;
-import android.graphics.PorterDuff.Mode;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.ImageView;
-import android.widget.SimpleAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class BlogDetailAdapter extends SimpleAdapter {
+public class BlogDetailAdapter extends ArrayAdapter<Post> {
 
-	private final LayoutInflater inflater;
+	BlogList activity = (BlogList) getContext();  
+	private ListView listView;
+	private List<Post> posts;
+	private AsyncImageLoader asyncImageLoader; 
 
+	public BlogDetailAdapter(Context context, List<Post> posts, ListView listView) {
+		super(context, 0, posts);
+		this.listView = listView;
+		this.posts = posts;
+		asyncImageLoader = new AsyncImageLoader();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View _selectView = convertView;
+		if (_selectView == null) {
+			LayoutInflater inflater = activity.getLayoutInflater();
+			_selectView = inflater.inflate(R.layout.microblog, null);
+		}
+		Post _post = getItem(position);
+		TextView tv = (TextView) _selectView.findViewById(R.id.blog_text);
+		tv.setText(Html.fromHtml(_post.getText()));
+		tv = (TextView) _selectView.findViewById(R.id.blog_from);
+		tv.setText(Html.fromHtml(_post.getCreated_at() + BlogList.activity.getResources().getString(R.string.from)
+				+ _post.getSource()));
+		ImageView iv = (ImageView) _selectView.findViewById(R.id.header);
+		//add a tag to allowed we can look for it and replace the picture
+		iv.setTag(_post.getUser().getProfile_image_url());
+		Bitmap cachedImage = asyncImageLoader.loadBitmap(_post.getUser()
+				.getProfile_image_url(), new ImageCallback() {
+			public void imageLoaded(Bitmap imageBitmap, String imageUrl) {
+				ImageView imageViewByTag = (ImageView) listView
+						.findViewWithTag(imageUrl);
+				if (imageViewByTag != null) {
+					imageViewByTag.setImageBitmap(imageBitmap);
+				}
+			}
+		});
+		if (cachedImage == null) {
+			//iv.setImageResource(R.drawable.header);
+		} else {
+			iv.setImageBitmap(cachedImage);
+			notifyDataSetChanged();
+		}
+		return _selectView;
+	}
+	
+	
+	/*
 	public BlogDetailAdapter(Context context,ArrayList<HashMap<String, String>> data){
 		super(context,data,R.layout.microblog,new String[]{"blog_text","blog_from"},new int[]{R.id.blog_text,R.id.blog_from});
 		inflater = LayoutInflater.from(context);
