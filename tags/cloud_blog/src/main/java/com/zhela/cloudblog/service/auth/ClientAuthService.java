448122e9ad107e4c99f8125745b1f5327e3919f9
yan925@gmail.com <yan925@gmail.com@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-10-23 07:02:53 +0000

    add device static

--- tags/cloud_blog/src/main/java/com/zhela/cloudblog/service/auth/ClientAuthService.java
+++ tags/cloud_blog/src/main/java/com/zhela/cloudblog/service/auth/ClientAuthService.java
@@ -1,7 +1,10 @@
 package com.zhela.cloudblog.service.auth;
 
+import com.zhela.cloudblog.rest.auth.AuthResource;
+
 public interface ClientAuthService {
 
 	public String getSerialID(String appkey,String sessionid);
 	public boolean checkSerialID(String enc_serialid, String serialid);
+	public void insertDeviceLog(AuthResource.DeviceStatus status,String sessionId,int action);
 }
