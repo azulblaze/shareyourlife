ecd995bd5f6eeea42526b32eaa390239bc97eaa1
yan925@gmail.com <yan925@gmail.com@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-11-25 05:43:24 +0000

    fix configure load error

--- tags/cloud_blog/src/main/java/com/zhela/cloudblog/util/SystemConfig.java
+++ tags/cloud_blog/src/main/java/com/zhela/cloudblog/util/SystemConfig.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import com.zhela.cloudblog.rest.BaseResource;
+
 /**
  * <code>SystemConfig.java</code>
  * @version 1.0, 2009-8-3
@@ -14,7 +16,7 @@ public class SystemConfig {
 	static{
 		Properties properties = new Properties();
 		try {
-			properties.load(new FileInputStream("config.properties"));
+			properties.load(new FileInputStream(BaseResource.ROOT_PATH+"WEB-INF/classes/config.properties"));
 			domain = properties.getProperty("domain");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
