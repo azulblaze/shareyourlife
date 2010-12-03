c9c527fe29b1a857018a28af635c7cc3793fa4d0
yan925 <yan925@gmail.com>
2010-12-03 14:43:29 +0000

    add struts and edit comment plugin css

---
+++ tags/cloud_blog/src/main/java/com/zhela/cloudblog/action/BaseAction.java
@@ -0,0 +1,74 @@
+package com.zhela.cloudblog.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * <code>BaseAction.java</code> is the interface of outputting the logs.
+ * 
+ * @author  Damon Chen
+ * @version 1.0, 2010-6-9
+ */
+@SuppressWarnings("serial")
+public abstract class BaseAction extends ActionSupport {
+	
+	protected final static String ROOT_PARENT_ID = "0";
+	protected String urlBack;
+	protected String JSON = "json";
+	public String getUrlBack() {
+		return urlBack;
+	}
+
+	public String getRootPath(){
+		return ServletActionContext.getServletContext().getRealPath("/");
+	}
+	
+	public void setValue(String key, Object value){
+		ServletActionContext.getValueStack(getHttpServletRequest()).set(key, value);
+	}
+	
+	public HttpSession getHttpSession(){
+		return ServletActionContext.getRequest().getSession();
+	}
+	
+	public HttpSession getHttpServletSession(boolean arg0){
+		return ServletActionContext.getRequest().getSession(arg0);
+	}	
+	
+	public HttpServletRequest getHttpServletRequest(){
+		return ServletActionContext.getRequest();	
+	}
+	
+	public HttpServletResponse getHttpServletResponse(){
+		return ServletActionContext.getResponse();	
+	}
+
+	public String getRequestParameter(String key) {
+		return ServletActionContext.getRequest().getParameter(key);
+	}
+	
+	public String[] getRequestParameterValues(String key) {
+		return ServletActionContext.getRequest().getParameterValues(key);
+	}
+	
+	public void clearSession(String key){
+		ServletActionContext.getRequest().getSession().removeAttribute(key);
+	}
+	
+	public void saveSession(String key,Object value){
+		ServletActionContext.getRequest().getSession().setAttribute(key, value);
+	}
+	
+	public Object getSession(String key){
+		return ServletActionContext.getRequest().getSession().getAttribute(key);
+	}
+	
+	protected void loadUrlBack(){
+		this.urlBack = ServletActionContext.getRequest().getHeader("Referer");
+	}
+}
---
+++ tags/cloud_blog/src/main/java/com/zhela/cloudblog/action/main/UserAction.java
@@ -0,0 +1,12 @@
+package com.zhela.cloudblog.action.main;
+
+import com.zhela.cloudblog.action.BaseAction;
+
+@SuppressWarnings("serial")
+public class UserAction extends BaseAction{
+
+	public String postUser() throws Exception{
+		return SUCCESS;
+	}
+	
+}
