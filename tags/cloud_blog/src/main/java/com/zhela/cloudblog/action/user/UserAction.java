e54616992fb9cb59e4a69d4dd9e15ac9677a0828
yan925@gmail.com <yan925@gmail.com@9d8c4ca6-7f7e-11de-b041-0b9255f66686>
2010-12-08 14:23:27 +0000

    rebuild the code

---
+++ tags/cloud_blog/src/main/java/com/zhela/cloudblog/action/user/UserAction.java
@@ -0,0 +1,27 @@
+package com.zhela.cloudblog.action.user;
+
+import com.zhela.cloudblog.action.BaseAction;
+
+@SuppressWarnings("serial")
+public class UserAction extends BaseAction{
+
+	public String postUser() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String deleteUser() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String getUser() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String updatePassword() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String updateHeader() throws Exception{
+		return SUCCESS;
+	}
+}
