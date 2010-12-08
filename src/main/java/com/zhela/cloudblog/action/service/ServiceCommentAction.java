2a059c965af757568e375ab0e66d734ef10cd87e
yan925 <yan925@gmail.com>
2010-12-08 14:23:27 +0000

    rebuild the code

---
+++ src/main/java/com/zhela/cloudblog/action/service/ServiceCommentAction.java
@@ -0,0 +1,31 @@
+package com.zhela.cloudblog.action.service;
+
+import com.zhela.cloudblog.action.BaseAction;
+
+@SuppressWarnings("serial")
+public class ServiceCommentAction extends BaseAction{
+
+	public String status() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String myComments() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String postService() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String deleteService() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String updateStatus() throws Exception{
+		return SUCCESS;
+	}
+	
+	public String getComments()throws Exception{
+		return SUCCESS;
+	}
+}
